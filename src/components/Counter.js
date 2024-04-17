5e510f411fdce9564ff886233dee812db360f9ef
atakansensel <atakansensel@gmail.com>
2024-04-17 21:59:08 +0300

    first commit

---
+++ src/components/Counter.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react';
+
+function Counter() {
+   const [count,setCounter] = useState(0);
+   const [username,setUserName] = useState("Atakan")
+
+   useEffect(()=>{
+    console.log(count);
+
+    return () => {
+        // Cleanup subscription
+        console.log("ssssss");
+    };
+   },[count]);
+
+  const Increment=()=>{
+    setCounter(count+1);
+  }
+
+  const Decrement=()=>{
+    setCounter(count-1);
+  }
+  
+  const handleOnChange=(event)=>{
+    setUserName(event.target.value);
+  }
+  return (
+    <div>
+      <p>{username}</p>
+      <input value={username} onChange={handleOnChange}></input>
+      <p>{count}</p>
+      <button onClick={Increment}>
+        +
+      </button>
+      <button onClick={Increment}>
+        
+      </button>
+      <button onClick={Decrement}>
+        -
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
